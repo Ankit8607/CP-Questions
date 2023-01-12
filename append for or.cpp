de7809fb82990876cf75a38171f13e013cea0227
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-01-12 12:23:41 +0530

    codechef

--- append for or.cpp
+++ append for or.cpp
@@ -3,6 +3,7 @@ using namespace std;
 const int mod= 10000007;
 #define endl "\n"
 #define F first
+#define int long long
 #define S second
 #define vi vector<int>
 #define ii pair<int,int>
@@ -11,12 +12,28 @@ const int mod= 10000007;
 #define tracearray(arr,n)  cout<<#arr<<endl;for(int i=0;i<n;i++)cout<<arr[i]<<" ";cout<<endl;
 
 void solve(){
-    cout<<(3<<1);
+    int n,y; cin>>n>>y;
+    int t=0;
+    for(int i=0;i<n;i++){
+        int a; cin>>a;
+        t|=a;
+    }
+    int ans=0;
+    for(int i=0;i<=20;i++){
+        if((y&(1LL<<i))==0){
+            if((t&(1LL<<i))){
+                cout<<-1<<endl;
+                return;
+            }
+        }
+        else if((t&(1LL<<i))==0) ans|=(1LL<<i);
+    }
+    cout<<ans<<endl;
 }
 
 signed main(){
     IOS
-    // int t=1; cin/>>t; while(t--) 
+    int t=1; cin>>t; while(t--) 
     solve();
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -9,27 +9,29 @@ const int mod= 10000007;
 #define all(a) a.begin(),a.end()
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define tracearray(arr,n)  cout<<#arr<<endl;for(int i=0;i<n;i++)cout<<arr[i]<<" ";cout<<endl;
+int n,m,k;
+
+int rec(int i,int j,vector<vector<int>>& arr,vector<vector<int>>&dp){
+    if(i>=n || j>=m) return 1e9;
+    if(i==n-1 and j==m-1) return 0;
+
+    if(dp[i][j]!=-1) return dp[i][j];
+
+    int ans=1+min(rec(i+1,j,arr,dp),rec(i,j+1,arr,dp));
+    if(arr[i][j]) ans=min(ans,1+rec(i+1,j+1,arr,dp));
+
+    return dp[i][j]=ans; 
+}
 
 void solve(){
-    int n,k,sum; cin>>n>>k>>sum;
-    vector<int>ans;
-    while(n--){
-        if((sum%k)==0) ans.push_back(0);
-        else if(((sum-1)%k)==0) sum-=1,ans.push_back(1);
-        else if(((sum+1)%k)==0) sum+=1,ans.push_back(-1);
-        else{
-            cout<<-2<<endl;
-            return;
-        }
-        sum/=k;
+    cin>>n>>m>>k;
+    vector<vector<int>>arr(n,vector<int>(m,0));
+    for(int i=0;i<k;i++){
+        int a,b; cin>>a>>b;
+        arr[a-1][b-1]=1;
     }
-    
-    if(sum){
-        cout<<-2<<endl;
-        return;
-    }
-    for(auto &i:ans) cout<<i<<" ";
-    cout<<endl;
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+    cout<<rec(0,0,arr,dp)<<endl;
 }
 
 signed main(){
