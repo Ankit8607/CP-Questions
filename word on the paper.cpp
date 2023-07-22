d1a19906ef5fa50ee551b766ac5db08019aef9aa
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-07-22 08:27:45 +0530

    codeforces

---
+++ word on the paper.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mod= 10000007;
+#define endl "\n"
+#define F first
+#define S second
+#define vi vector<int>
+#define ii pair<int,int>
+#define all(a) a.begin(),a.end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define tracearray(arr,n)  cout<<#arr<<endl;for(int i=0;i<n;i++)cout<<arr[i]<<" ";cout<<endl;
+
+void solve(){
+    string ans;
+    for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+            char a; cin>>a;
+            if(a!='.') ans+=a;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+signed main(){
+    IOS
+    int t=1; cin>>t; while(t--) 
+    solve();
+    return 0;
+}
