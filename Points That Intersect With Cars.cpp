93054315e9274793b5c24b52d207c050397c574f
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-09-10 09:36:12 +0530

    Create Points That Intersect With Cars.cpp

---
+++ Points That Intersect With Cars.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/contest/weekly-contest-362/problems/points-that-intersect-with-cars/
+
+class Solution {
+public:
+    int numberOfPoints(vector<vector<int>>& nums) {
+        vector<int>arr(101,0);
+        for(auto i:nums){
+            for(int j=i[0];j<=i[1];j++) arr[j]=1;
+        }
+        int cnt=0;
+        for(int i=0;i<101;i++) cnt+=arr[i];
+        return cnt;
+    }
+};
