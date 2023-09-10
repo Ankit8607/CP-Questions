3515d05aa24dcf729ec8ec92ef67e565d70e930e
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-09-10 09:53:27 +0530

    Create Minimum Moves to Spread Stones Over Grid.cpp

---
+++ Minimum Moves to Spread Stones Over Grid.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/minimum-moves-to-spread-stones-over-grid/description/
+
+class Solution {
+private:
+    vector<vector<int>> xtra, zero;
+    int ret;
+    void solve(int i, int count) {
+        if(i >= zero.size()) {
+            ret = min(ret, count);
+            return;
+        }
+        for(int k = 0; k < xtra.size(); k++) {
+            if(xtra[k][2] == 1)    continue;
+            xtra[k][2]--;
+            solve(i + 1, abs(xtra[k][0] - zero[i][0]) + abs(xtra[k][1] - zero[i][1]) + count);
+            xtra[k][2]++;
+        }
+    }
+public:
+    int minimumMoves(vector<vector<int>>& grid) {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(grid[i][j] == 0)
+                    zero.push_back({i, j});
+                else if(grid[i][j] > 1)
+                    xtra.push_back({i, j, grid[i][j]});
+            }
+        }
+        // if(zero.size() == 0)   return 0;
+        ret = INT_MAX;
+        solve(0, 0);
+        return ret;
+    }
+};
