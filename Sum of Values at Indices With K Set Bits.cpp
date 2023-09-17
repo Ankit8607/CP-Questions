0d65b47e3f09000e658986ec1545c1147c790120
Ankit Kumar <83530113+Ankit8607@users.noreply.github.com>
2023-09-17 09:43:20 +0530

    Create Sum of Values at Indices With K Set Bits.cpp

---
+++ Sum of Values at Indices With K Set Bits.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/description/
+
+
+class Solution {
+public:
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int ans=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=0;j<20;j++){
+                if(((1<<j)&i)>0) cnt++;
+            }
+            if(cnt==k) ans+=nums[i];
+        }
+        return ans;
+    }
+};
